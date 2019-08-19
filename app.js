cdc85e656e1c3bca4f017936c758eaf3c43693e5
Oleg Milkin <oleg.milkin@flexaspect.com>
2019-08-19 20:03:03 +0300

    fixes

--- app.js
+++ app.js
@@ -5,7 +5,30 @@ var ComponentUsers = {
       }
     },
     template: '#component-users-template'
-}
+};
+
+var ComponentUserForm = {
+    props: {
+        user: {
+            type: Object
+        }
+    },
+    template: '<form>\n' +
+        '  <div class="form-group">\n' +
+        '    <label for="formGroupExampleInput1">Enter new name</label>\n' +
+        '    <input type="text" class="form-control" id="formGroupExampleInput1" :placeholder="user.name">\n' +
+        '  </div>\n' +
+        '  <div class="form-group">\n' +
+        '    <label for="formGroupExampleInput2">Enter new surname</label>\n' +
+        '    <input type="text" class="form-control" id="formGroupExampleInput2" :placeholder="user.surname">\n' +
+        '  </div>\n' +
+        '  <div class="form-group">\n' +
+        '    <label for="formGroupExampleInput3">Enter new patronymic</label>\n' +
+        '    <input type="text" class="form-control" id="formGroupExampleInput3" :placeholder="user.patronymic">\n' +
+        '  </div>\n' +
+        '<button type="submit" class="btn btn-primary">Save</button>' +
+        '</form>'
+};
 
 var app = new Vue({
     el: '#app',
@@ -39,6 +62,7 @@ var app = new Vue({
         }
     },
     components: {
-        'component-users': ComponentUsers
+        'component-users': ComponentUsers,
+        'user-form': ComponentUserForm
     }
 });
