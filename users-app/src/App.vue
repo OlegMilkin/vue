8818e6f879c0a07c702df87aa66057fcd92390b6
Oleg Milkin <oleg.milkin@flexaspect.com>
2019-08-22 20:52:05 +0300

    first commit

---
+++ users-app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" v-cloak>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="/"><img alt="Vue logo" src="@/assets/logo.png" width="30" /></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/users">Users</router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/edit">Edit</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+
+      <router-view />
+
+    </main><!-- /.container -->
+  </div>
+</template>
+
+<style>
+  #app {
+    margin-top: 60px;
+  }
+
+  [v-clock] {
+    display: none;
+  }
+</style>
---
+++ users-app/src/views/Users.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <h2>Список пользователей</h2>
+      <div v-if="!users.length" class="alert alert-warning">
+        Загрузка...
+      </div>
+      <component-users v-else :users="users" :key="users.id"></component-users>
+    </div>
+</template>
+
+<script>
+  import ComponentUsers from '@/components/ComponentUsers.vue';
+  import axios from 'axios';
+
+  export default {
+    name: 'UsersPage',
+    components: {
+      'component-users': ComponentUsers
+    },
+    data: function(){
+      return {
+        users: []
+      }
+    },
+    mounted: function() {
+      this.loadData();
+    },
+    methods: {
+      loadData: function() {
+        axios.get("http://localhost:3000/users/")
+                .then(response => response.data)
+                .then(response => (this.users = response))
+                .catch(error => console.error(error))
+      }
+    }
+  }
+</script>
+
